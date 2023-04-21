023b6273bd50eea6b825d2ef2ff5ce09875629de
almos <gwl411@163.com>
2023-04-21 17:35:18 +0800

    创建数据库,填入数据

--- create_database.py
+++ create_database.py
@@ -14,30 +14,30 @@ import sqlite3
 conn = sqlite3.connect('ncbi_resource.sqlite')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE assembly_summary_refseq
-             (assembly_accession text PRIMARY KEY   NOT NULL,
-             bioproject text,
-             biosample text,
-             wgs_master text,
-             refseq_category text,
-             taxid int,
-             species_taxid int,
-             organism_name text,
-             infraspecific_name text,
-             isolate text,
-             version_status text,
-             assembly_level text,
-             release_type text,
-             genome_rep text,
-             seq_rel_date text,
-             asm_name text,
-             submitter text,
-             gbrs_paired_asm text,
-             paired_asm_comp text,
-             ftp_path text,
-             excluded_from_refseq text,
-             relation_to_type_material text,
-             asm_not_live_date text)''')
+# c.execute('''CREATE TABLE assembly_summary_refseq
+#              (assembly_accession text PRIMARY KEY   NOT NULL,
+#              bioproject text,
+#              biosample text,
+#              wgs_master text,
+#              refseq_category text,
+#              taxid int,
+#              species_taxid int,
+#              organism_name text,
+#              infraspecific_name text,
+#              isolate text,
+#              version_status text,
+#              assembly_level text,
+#              release_type text,
+#              genome_rep text,
+#              seq_rel_date text,
+#              asm_name text,
+#              submitter text,
+#              gbrs_paired_asm text,
+#              paired_asm_comp text,
+#              ftp_path text,
+#              excluded_from_refseq text,
+#              relation_to_type_material text,
+#              asm_not_live_date text)''')
 
 
 with open('assembly_summary_refseq.txt', 'r') as f:
